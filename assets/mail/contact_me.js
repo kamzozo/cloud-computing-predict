8e1d8b82fec626b13c73bda4146ec0de8eecdfa8
kamzozo <84721922+kamzozo@users.noreply.github.com>
2022-10-18 16:17:14 +0200

    Update contact_me.js

--- assets/mail/contact_me.js
+++ assets/mail/contact_me.js
@@ -24,7 +24,7 @@ $(function () {
             var json = {name: name, phone: phone, email: email, message: message}
             $.ajax({
                 // --- CHANGE THIS LINE TO YOUR OWN API GATEWAY  -------- 
-                url: "https://1ml39xqozh.execute-api.eu-west-1.amazonaws.com/lambda-function",
+                url: " https://1ml39xqozh.execute-api.eu-west-1.amazonaws.com/lambda-function",
                 // ------------------------------------------------------  
                 type: "POST",
                 data: JSON.stringify(json),
